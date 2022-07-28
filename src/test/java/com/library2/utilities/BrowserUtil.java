957d8c8b903debc2b276e2f0d5d12d2c4c5d4131
Eliza Turgunbaeva <elizaturgunbaeva@gmail.com>
2022-07-28 15:11:14 -0400

    Eliza commit

--- src/test/java/com/library2/utilities/BrowserUtil.java
+++ src/test/java/com/library2/utilities/BrowserUtil.java
@@ -1,11 +1,37 @@
 package com.library2.utilities;
 
+import org.junit.Assert;
+
+import java.util.Set;
+
 public class BrowserUtil {
 
     /** 1. ACTION METHODS: */
 
 
+
+
     /** 2. ASSERTION METHODS: */
+    public static void switchWindowAndVerify(String expectedInUrl,String expectedTitle){
+
+        Set<String> allWindowHandles = Driver.getDriver().getWindowHandles();
+
+        for (String eachWindow : allWindowHandles) {
+            // driver.switchTo().window(eachWindow); syntax will switch each window and driver will be able to see each window
+            Driver.getDriver().switchTo().window(eachWindow);
+            //  System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl());
+
+            if(Driver.getDriver().getCurrentUrl().contains(expectedInUrl)){
+                break;
+            }
+        }
+
+//        5. Assert:Title contains “Etsy”
+        String actualTitle = Driver.getDriver().getTitle();
+
+        Assert.assertTrue("Title verification failed!",actualTitle.contains(expectedTitle));
+    }
+
 
 
     /** 3. SUPPORTING METHODS: */
