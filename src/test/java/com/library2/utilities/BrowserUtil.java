5a86f35d2cabd5a8f343003aeb80af8d977e81b4
Eliza Turgunbaeva <elizaturgunbaeva@gmail.com>
2022-07-28 15:12:32 -0400

    Eliza commit

--- src/test/java/com/library2/utilities/BrowserUtil.java
+++ src/test/java/com/library2/utilities/BrowserUtil.java
@@ -1,6 +1,10 @@
 package com.library2.utilities;
 
 import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.Set;
 
@@ -30,6 +34,25 @@ public class BrowserUtil {
         String actualTitle = Driver.getDriver().getTitle();
 
         Assert.assertTrue("Title verification failed!",actualTitle.contains(expectedTitle));
+    }
+
+    public static void verifyTitle(WebDriver driver, String expectedTitle){
+
+        Assert.assertEquals(driver.getTitle(),expectedTitle);
+
+    }
+
+
+    public static void waitForInvisibilityOf(WebElement element){
+
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),10);
+        wait.until(ExpectedConditions.invisibilityOf(element));
+
+    }
+
+
+    public static void verifyURLContains(String expectedInURL){
+        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains(expectedInURL));
     }
 
 
