436f73069e6b364455069e725970bcdcd83c067a
BLSoftUser <bluelaser.soft@gmail.com>
2022-08-09 16:23:26 -0400

    1. updated gitignore file, Driver class, FailedClassRunner class 2. added and corrected plugins for parallel testing in pom.xml 3. changed the history_creating_Framework file
    PS: in Driver class- changed the object from driver into driverPool

--- src/test/java/com/library2/runner/FailedClassRunner.java
+++ src/test/java/com/library2/runner/FailedClassRunner.java
@@ -4,12 +4,12 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "@target/rerun.txt",
-        glue="com/libray2/step_definitions"
+    @RunWith(Cucumber.class)
+    @CucumberOptions(
+            features = "@target/rerun.txt",
+            glue="com/libray2/step_definitions"
 
-)
+    )
 
 
 public class FailedClassRunner {
